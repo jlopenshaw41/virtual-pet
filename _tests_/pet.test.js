e02a1e0117efe85c772d933f838e8b0035e3e78f
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2020-11-04 11:55:47 +0000

    added age, hunger and fitness functionality to growUp() prototype method

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -12,9 +12,28 @@ describe('constructor', () => {
         const pet = new Pet('Fido');
         expect(pet.age).toBe(0);
     });
+    it('has an initial hunger value of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toBe(0);
+    });
+    it('has an intial fitness value of 10', () => {
+        const pet = new Pet('Fido');
+        expect(pet.fitness).toBe(10);
+    })
     it('increments age by 1', () => {
         const pet = new Pet('Fido');
         pet.growUp();
         expect(pet.age).toBe(1);
-    } )
+    });
+    it('increases hunger by 5', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toBe(5);
+    });
+    it('decreases fitness by 3', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toBe(7);
+    })
+    
 });
