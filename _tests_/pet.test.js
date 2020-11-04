8bc58e6d5f20a08f8e54b47e1de349d1d677a93a
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2020-11-04 13:13:15 +0000

    fixed beforeEach() - declared variable outside of scope of beforeEach to make it accessible to each test

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -1,13 +1,11 @@
 const Pet = require('../src/pet');
 
-const createNewInstance = () => {
-    const pet = new Pet('Fido');
-}
-
 describe('constructor', () => {
-
-    beforeAll(() => {
-        createNewInstance();
+    
+    let pet;
+    
+    beforeEach(() => {
+        pet = new Pet('Fido');
     });
 
     it('returns an object', () => {
@@ -15,39 +13,32 @@ describe('constructor', () => {
     });
 
     it('sets the value of the name property', () => {
-        // const pet = new Pet('Fido');
         expect(pet.name).toBe('Fido');
     });
 
     it('has an initial age value of 0', () => {
-        // const pet = new Pet('Fido');
         expect(pet.age).toBe(0);
     });
 
     it('has an initial hunger value of 0', () => {
-        // const pet = new Pet('Fido');
         expect(pet.hunger).toBe(0);
     });
 
     it('has an intial fitness value of 10', () => {
-        // const pet = new Pet('Fido');
         expect(pet.fitness).toBe(10);
     });
 
     it('increments age by 1', () => {
-        // const pet = new Pet('Fido');
         pet.growUp();
         expect(pet.age).toBe(1);
     });
 
     it('increases hunger by 5', () => {
-        // const pet = new Pet('Fido');
         pet.growUp();
         expect(pet.hunger).toBe(5);
     });
 
     it('decreases fitness by 3', () => {
-        // const pet = new Pet('Fido');
         pet.growUp();
         expect(pet.fitness).toBe(7);
     });
